888f842b4ae8d31cd9267f1b4faa1ff1a49e5320
Fox <31753655+xXxsomebodyoncetoldmexXx@users.noreply.github.com>
2021-04-28 01:18:02 +0700

    Finish setup enviroment

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,21 @@
-import express from "express";
+import express, { Application, Request, Response } from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import * as winston from 'winston';
+import * as expressWinston from 'express-winston';
+import debug from 'debug';
 
-const app: express.Application = express();
+dotenv.config();
 
-console.log("Hello world");
+const app: Application = express();
+const port: number = Number(process.env.PORT) || 8000;
+const debugLog: debug.IDebugger = debug('app');
+
+app.use(express.json());
+app.use(cors());
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello world');
+});
+
+app.listen(port);
